5030e3631a5134f58b48bb4475916ec1f7981357
Naupaka Zimmerman <naupaka@gmail.com>
2017-06-20 12:16:04 -0600

    Finish generalizing script

--- code/make_HS_histogram.py
+++ code/make_HS_histogram.py
@@ -12,7 +12,10 @@ import warnings
 warnings.filterwarnings('ignore')
 
 filename = sys.argv[1]
-band_of_interest = sys.argv[2]
+
+# force the band we want to be an integer, and convert to python index
+band_of_interest = int(sys.argv[2]) - 1
+
 outfile_name = sys.argv[3]
 
 if("--verbose" in sys.argv):
@@ -38,7 +41,7 @@ def list_dataset(name,node):
     if isinstance(node, h5py.Dataset):
         print(name)
 
-f.visititems(list_dataset)
+if(DEBUG): f.visititems(list_dataset)
 
 
 # We can display the name, shape, and type of each of these datasets using the ```ls_dataset``` function defined below, which is also called with ```visititems```: 
@@ -50,7 +53,7 @@ def ls_dataset(name,node):
     if isinstance(node, h5py.Dataset):
         print(node)
     
-f.visititems(ls_dataset)
+if(DEBUG): f.visititems(ls_dataset)
 
 
 # Now that we see the general structure of the hdf5 file, let's take a look at some of the information that is stored inside. Let's start by extracting the reflectance data, which is nested under SERC/Reflectance/Reflectance_Data.  
@@ -58,7 +61,7 @@ f.visititems(ls_dataset)
 # In[6]:
 
 serc_refl = f['SERC']['Reflectance']
-print(serc_refl)
+if(DEBUG): print(serc_refl)
 
 
 # The two members of the HDF5 group /SERC/Reflectance are *Metadata* and *Reflectance_Data*. Let's save the reflectance data as the variable serc_reflArray:
@@ -66,7 +69,7 @@ print(serc_refl)
 # In[7]:
 
 serc_reflArray = serc_refl['Reflectance_Data']
-print(serc_reflArray)
+if(DEBUG): print(serc_reflArray)
 
 
 # We can extract the shape as follows: 
@@ -74,7 +77,7 @@ print(serc_reflArray)
 # In[8]:
 
 refl_shape = serc_reflArray.shape
-print('SERC Reflectance Data Dimensions:',refl_shape)
+if(DEBUG): print('SERC Reflectance Data Dimensions:',refl_shape)
 
 
 # This corresponds to (y,x,bands), where (x,y) are the dimensions of the reflectance array in pixels (1m x 1m). All NEON hyperspectral data contains 426 wavelength bands. Let's take a look at the wavelength values:
@@ -84,15 +87,15 @@ print('SERC Reflectance Data Dimensions:',refl_shape)
 #View wavelength information and values
 
 wavelengths = serc_refl['Metadata']['Spectral_Data']['Wavelength']
-print(wavelengths)
+if(DEBUG): print(wavelengths)
 # print(wavelengths.value)
 # Display min & max wavelengths
-print('min wavelength:', np.amin(wavelengths),'nm')
-print('max wavelength:', np.amax(wavelengths),'nm')
+if(DEBUG): print('min wavelength:', np.amin(wavelengths),'nm')
+if(DEBUG): print('max wavelength:', np.amax(wavelengths),'nm')
 
 #show the band width 
-print('band width =',(wavelengths.value[1]-wavelengths.value[0]),'nm')
-print('band width =',(wavelengths.value[-1]-wavelengths.value[-2]),'nm')
+if(DEBUG): print('band width =',(wavelengths.value[1]-wavelengths.value[0]),'nm')
+if(DEBUG): print('band width =',(wavelengths.value[-1]-wavelengths.value[-2]),'nm')
 
 
 # The wavelengths recorded range from 383.66 - 2511.94 nm, and each band covers a range of ~5 nm. Now let's extract spatial information, which is stored under SERC/Reflectance/Metadata/Coordinate_System/Map_Info:
@@ -100,7 +103,7 @@ print('band width =',(wavelengths.value[-1]-wavelengths.value[-2]),'nm')
 # In[10]:
 
 serc_mapInfo = serc_refl['Metadata']['Coordinate_System']['Map_Info']
-print('SERC Map Info:\n',serc_mapInfo.value)
+if(DEBUG): print('SERC Map Info:\n',serc_mapInfo.value)
 
 
 # **Notes:**
@@ -116,7 +119,7 @@ print('SERC Map Info:\n',serc_mapInfo.value)
 #First convert mapInfo to a string, and divide into separate strings using a comma seperator
 mapInfo_string = str(serc_mapInfo.value) #convert to string
 mapInfo_split = mapInfo_string.split(",") #split the strings using the separator "," 
-print(mapInfo_split)
+if(DEBUG): print(mapInfo_split)
 
 
 # Now we can extract the spatial information we need from the map info values, convert them to the appropriate data types (eg. float) and store it in a way that will enable us to access and apply it later: 
@@ -125,7 +128,7 @@ print(mapInfo_split)
 
 #Extract the resolution & convert to floating decimal number
 res = float(mapInfo_split[5]),float(mapInfo_split[6])
-print('Resolution:',res)
+if(DEBUG): print('Resolution:',res)
 
 #Extract the upper left-hand corner coordinates from mapInfo
 xMin = float(mapInfo_split[3]) 
@@ -138,7 +141,7 @@ yMin = yMax - (refl_shape[0]*res[1])
 # print('xMin:',xMin) ; print('xMax:',xMax) 
 # print('yMin:',yMin) ; print('yMax:',yMax) 
 serc_ext = (xMin, xMax, yMin, yMax)
-print('serc_ext:',serc_ext)
+if(DEBUG): print('serc_ext:',serc_ext)
 
 #Can also create a dictionary of extent:
 serc_extDict = {}
@@ -146,18 +149,18 @@ serc_extDict['xMin'] = xMin
 serc_extDict['xMax'] = xMax
 serc_extDict['yMin'] = yMin
 serc_extDict['yMax'] = yMax
-print('serc_extDict:',serc_extDict)
+if(DEBUG): print('serc_extDict:',serc_extDict)
 
 
 # ## Extract a single band from the array
 
 # In[13]:
 
-b56 = serc_reflArray[:,:,55].astype(np.float)
-print('b56 type:',type(b56))
-print('b56 shape:',b56.shape)
-print('Band 56 Reflectance:\n',b56)
-# plt.hist(b56.flatten())
+my_band = serc_reflArray[:,:,band_of_interest].astype(np.float)
+if(DEBUG): print('b' + str(band_of_interest + 1) + ' type:', type(my_band))
+if(DEBUG): print('b' + str(band_of_interest + 1) + ' shape:', my_band.shape)
+if(DEBUG): print('Band ' + str(band_of_interest + 1) + ' Reflectance:\n', my_band)
+# plt.hist(my_band.flatten())
 
 
 # ## Apply the scale factor and data ignore value
@@ -175,19 +178,20 @@ print('Band 56 Reflectance:\n',b56)
 #View and apply scale factor and data ignore value
 scaleFactor = serc_reflArray.attrs['Scale_Factor']
 noDataValue = serc_reflArray.attrs['Data_Ignore_Value']
-print('Scale Factor:',scaleFactor)
-print('Data Ignore Value:',noDataValue)
+if(DEBUG): print('Scale Factor:',scaleFactor)
+if(DEBUG): print('Data Ignore Value:',noDataValue)
 
-b56[b56==int(noDataValue)]=np.nan
-b56 = b56/scaleFactor
-print('Cleaned Band 56 Reflectance:\n',b56)
+my_band[my_band==int(noDataValue)]=np.nan
+my_band = my_band/scaleFactor
+print('Cleaned Band ' + str(band_of_interest + 1) +' Reflectance:\n', my_band)
 
 
 # ## Plot histogram of reflectance data values
 
 # In[15]:
 
-plt.hist(b56[~np.isnan(b56)],50);
-plt.title('Histogram of SERC Band 56 Reflectance')
+plt.hist(my_band[~np.isnan(my_band)], 50);
+plt.title('Histogram of SERC Band ' + str(band_of_interest + 1) +' Reflectance')
 plt.xlabel('Reflectance'); plt.ylabel('Frequency')
+plt.savefig(outfile_name)
 
